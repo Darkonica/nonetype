1ea8c68d75d5b5cf747ac601da1382929b47d4e4
Vladimir Novichikhin <darkonica@gmail.com>
2019-03-05 12:57:23 +0300

    Initial

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,18 @@
+module.exports = function() {
+  $.gulp.task('watch', function() {
+    return new Promise((res, rej) => {
+      $.watch(['./src/views/**/*.pug', '!./src/views/blocks/*.pug'], $.gulp.series('pug'));
+      $.watch('./src/styles/*.scss', './src/styles/*/*.scss', $.gulp.series('styles'));
+      $.watch(
+        [
+          './src/img/**/*.{jpg,jpeg,png,gif}',
+          '!./src/img/icons/svg/*.svg',
+          '!./src/img/favicons/*.{jpg,jpeg,png,gif}',
+        ],
+        $.gulp.series('images')
+      );
+      $.watch('./src/js/**/*.js', $.gulp.series('scripts'));
+      res();
+    });
+  });
+};
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+let hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', hamburgerToggle);
+
+function hamburgerToggle() {
+  this.classList.toggle('is-active');
+  document.querySelector('.mobile-nav').classList.toggle('is-active');
+}
